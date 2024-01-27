e42e9520b2185c2feffbd84d3108b1e32dc80028
mvoelcker <mi.voelcker@gmail.com>
2024-01-27 17:30:54 +0100

    Package version update

--- versioned_fastapi/__init__.py
+++ versioned_fastapi/__init__.py
@@ -1,6 +1,6 @@
 """Versioned FastAPI simplifies the versioning of FastAPI web applications."""
 
-__version__ = "0.0.1"
+__version__ = "1.0.0"
 
 
 from .version_fastapi import FastApiVersioner, version
